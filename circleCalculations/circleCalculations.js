cd400525f4800ddb75606bad67e892b970144eae
trigger <trigger@anniecannons.com>
2020-11-03 10:33:46 -0800

    trigger CircleCalculations update

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -2,9 +2,21 @@
 
 //Bonus 1: Write the answer like this "The area of the circle is ___ and the perimeter of the circle is ____"
 
-//Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
+//Bonus 2: Make both numbers whole integers (aka round to the nearest integer)
 
-function circleCalculation () {
-	//your code here
+var r = parseInt(prompt("What is the radius of the circle?"));
 
+function circleCalculation(r) {
+  var perimeter = Math.round(2 * Math.PI * r);
+  var area = Math.round(Math.PI * Math.pow(r, 2));
+  var sentance =
+    "The area of the circle is " +
+    perimeter +
+    " and the perimeter of the circle is " +
+    area +
+    ".";
+  console.log(sentance);
+}
+
+circleCalculation(r);
 circleCalculation(10);
